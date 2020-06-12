1bfb8747fde8ac7dd766087a2672369a8ee92898
ali.teeney <alistair.teeney@octoenergy.com>
2020-06-12 22:34:10 +1000

    Extend the decoder to allow returning single param
    For the NP paper, we only want to predict mean, and set the variance to
    be fixed. All the variance in the output sample functions will come from
    sampling z from the latent space.

--- neural_processes/decoders.py
+++ neural_processes/decoders.py
@@ -9,6 +9,8 @@ from neural_processes import base
 class Decoder(torch.nn.Module):
     def __init__(self, input_size: int, output_sizes: List[int]):
         super().__init__()
+        if output_sizes[-1] not in (1, 2):
+            raise ValueError("Final output should be size 1 (for just mu) or 2 (for mu & sigma)")
         self.mlp = base.MLP(input_size, output_sizes)
 
     def forward(self, x, r):
@@ -19,9 +21,14 @@ class Decoder(torch.nn.Module):
 
         out = self.mlp.forward(inp)
 
-        mu, sigma = out[:, :, 0], out[:, :, 1]
+        # If our final output size is 2, we're returning both mu and sigma
+        if self.mlp.final.out_features == 2:
+            mu, sigma = out[:, :, 0], out[:, :, 1]
 
-        # Make the sigma positive and bounded below at 0.1
-        sigma = 0.1 + 0.9 * F.softplus(sigma)
+            # Make the sigma positive and bounded below at 0.1
+            sigma = 0.1 + 0.9 * F.softplus(sigma)
 
-        return mu, sigma
+            return mu, sigma
+        # Otherwise, the output size is 1 so just return mu
+        else:
+            return out[:, :, 0]
